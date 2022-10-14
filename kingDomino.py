413e8afd80b2c186cd8c2d01110e0add552701e1
andreasHovaldt <andreas.hovaldt@gmail.com>
2022-10-14 16:15:42 +0200

    Sheesh

--- kingDomino.py
+++ kingDomino.py
@@ -48,6 +48,13 @@ class dominoTile:
         cv2.destroyAllWindows()
     
 
+#Create tileBoard with dominoTile class
+def tileBoard(image, y=5, x=5):
+    for ySquare in range(y):
+        for xSquare in range(x):
+            pass
+
+
 
 ############################################### MAIN CODE ###############################################
 
@@ -59,12 +66,52 @@ img12Eq = equalizeHistogram(img12copy)
 img4 = cv2.imread("King Domino dataset/Cropped and perspective corrected boards/4.jpg")
 img4copy = np.copy(img4)
 
+img20 = cv2.imread("King Domino dataset/Cropped and perspective corrected boards/20.jpg")
+img20copy = np.copy(img20)
 
-#Create tileBoard with dominoTile class
-def tileBoard(image, y=5, x=5):
-    for ySquare in range(y):
-        for xSquare in range(x):
-            pass
+############# Loading #############
+img12List = segmentImage(img12copy)
+img20List = segmentImage(img20copy)
+
+seaTile = img12List[2][4]
+fieldTile = img12List[0][0]
+forestTile = img12List[1][0]
+plainsTile = img12List[2][0]
+swampTile = img12List[4][0]
+mineTile = img20List[2][0]
+#testTile = img12List[2][1]
+
+
+############# Blurring #############
+#seaTileMedBlur = cv2.medianBlur(seaTile,99)
+seaTileGausBlur = cv2.GaussianBlur(seaTile,(99,99),99)
+fieldTileGausBlur = cv2.GaussianBlur(fieldTile,(99,99),99)
+forestTileGausBlur = cv2.GaussianBlur(forestTile,(99,99),99)
+plainsTileGausBlur = cv2.GaussianBlur(plainsTile,(99,99),99)
+swampTileGausBlur = cv2.GaussianBlur(swampTile,(99,99),99)
+mineTileGausBlur = cv2.GaussianBlur(mineTile,(99,99),99)
+#testTileGausBlur = cv2.GaussianBlur(testTile,(99,99),99)
+
+cv2.imwrite("C:/Users/Andreas/Desktop/blurredTiles/seaTile.png", seaTileGausBlur)
+cv2.imwrite("C:/Users/Andreas/Desktop/blurredTiles/fieldTile.png", fieldTileGausBlur)
+cv2.imwrite("C:/Users/Andreas/Desktop/blurredTiles/forestTile.png", forestTileGausBlur)
+cv2.imwrite("C:/Users/Andreas/Desktop/blurredTiles/plainsTile.png", plainsTileGausBlur)
+cv2.imwrite("C:/Users/Andreas/Desktop/blurredTiles/swampTile.png", swampTileGausBlur)
+cv2.imwrite("C:/Users/Andreas/Desktop/blurredTiles/mineTile.png", mineTileGausBlur)
+
+############# Subtract #############
+#difference = cv2.subtract(seaTileGausBlur, testTileGausBlur)
+#difference = cv2.subtract(testTileGausBlur, seaTileGausBlur)
+
+
+
+#cv2.imshow("Sea tile", seaTile)
+#cv2.imshow("Sea tile Med Blur", seaTileMedBlur)
+#cv2.imshow("Sea tile Gaus Blur", seaTileGausBlur)
+#cv2.imshow("Test tile Gaus Blur", testTileGausBlur)
+#cv2.imshow("Difference", difference)
+#cv2.waitKey()
+#cv2.destroyAllWindows()
             
 
 
--- kingDominoFunctions.py
+++ kingDominoFunctions.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import random
 
 def edgeDetection(image):
     imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
